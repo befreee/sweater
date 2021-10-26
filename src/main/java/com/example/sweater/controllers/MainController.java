6e1442ebb18b31e176a85c2f6dd3efb1178b46ca
befreee <ostretsovalexander@gmail.com>
2021-10-26 18:39:10 +0300

    fourth stage

--- src/main/java/com/example/sweater/controllers/MainController.java
+++ src/main/java/com/example/sweater/controllers/MainController.java
@@ -1,8 +1,10 @@
 package com.example.sweater.controllers;
 
 import com.example.sweater.domain.Message;
+import com.example.sweater.domain.User;
 import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -31,8 +33,10 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(@AuthenticationPrincipal User user,
+            @RequestParam String text,
+                      @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -1,11 +1,6 @@
 package com.example.sweater.domain;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -17,13 +12,30 @@ public class Message {
     private String text;
     private String tag;
 
+    @ManyToOne(fetch =FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
 
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User author) {
+        this.author = author;
         this.text = text;
         this.tag = tag;
+    }
+
+    public String getAuthorName(){
+        return author!= null? author.getUsername():"<none>";
+    }
+
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     public Integer getId() {
